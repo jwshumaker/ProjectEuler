62cd653a082c8382a9b96690bbf4ad6c80d0520e
Jedediah Shumaker <jed.shumaker@gmail.com>
2014-05-02 15:35:31 -0500

    Update Problem_10.py

--- Problem_10.py
+++ Problem_10.py
@@ -1,142 +1,46 @@
 '''
-Created on Apr 9, 2014
+Created on Apr 8, 2014
 
 @author: JWShumaker
 
 ProjectEuler.net
 Problem ID 10
-In the 20x20 grid below, four numbers along a diagonal line have been marked in red.
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+Find the sum of all the primes below two million.
 
-The product of these numbers is 26 x 63 x 78 x 14 = 1788696.
-
-What is the greatest product of four adjacent numbers in the same direction (up,
- down, left, right, or diagonally) in the 20x20 grid?
- 
 SOLUTION:
-We will read our grid in as a list.  Our program will depend on being given
- the height and width of the grid.  
- 
-Our solution algorithm will be a sequential search of diagonal left, right, down,
- diagonal down left and diagonal down right, starting in the top left of the grid.
- 
-Note that we don't bother searching up or either upwards diagonal.  Doing so would
- be redundant since we begin our search at the top.
+Since we are given an upper bound on the largest prime, we will use the Sieve of
+ Erotosthenes from Problem ID 5.
 '''
 
-# Let's make a class for our grid and it's operations.
-class ProductGrid:
-    def __init__(self, height, width, length, grid):
-        self.height = height
-        self.width = width
-        self.length = length
-        self.grid = grid
-    
-    # NOTE: Our grid is accessed with y,x coordinates.    
-    # Finds the left product.
-    def LeftProduct(self, column, row):
-        if column >= self.length - 1:     
-            product = 1
-            for i in range(0, self.length):
-                product *= self.grid[row][column - i]
-                
-            return product
-            
-        return 1
-    
-    # Finds the right product.
-    def RightProduct(self, column, row):
-        if column <= self.width - self.length:     
-            product = 1
-            for i in range(0, self.length):
-                product *= self.grid[row][column + i]
-                
-            return product
-            
-        return 1
-    
-    # Finds the down product.
-    def DownProduct(self, column, row):
-        if row <= self.height - self.length:     
-            product = 1
-            for i in range(0, self.length):
-                product *= self.grid[row + i][column]
-                
-            return product
-            
-        return 1
-    
-    # Finds the diagonal left product.
-    def DiagonalLeftProduct(self, column, row):
-        if row <= self.height - self.length and column >= self.length - 1:     
-            product = 1
-            for i in range(0, self.length):
-                product *= self.grid[row + i][column - i]
-                
-            return product
-            
-        return 1
-    
-    # Finds the diagonal right product.
-    def DiagonalRightProduct(self, column, row):
-        if row <= self.height - self.length and column <= self.width - self.length:     
-            product = 1
-            for i in range(0, self.length):
-                product *= self.grid[row + i][column + i]
-                
-            return product
-            
-        return 1
-    
-    # Finds the five products and returns largest.
-    def FindProduct(self, column, row):
-        return max(self.LeftProduct(column, row), self.RightProduct(column, row), 
-                   self.DownProduct(column, row), self.DiagonalLeftProduct(column, row), 
-                   self.DiagonalRightProduct(column, row))
-                
-    
-# A method to process a grid and find the largest product of N elements.
-def FindLargestGridProduct(height, width, length, grid):
-    largest_product = 1
-    
-    # Turn our grid into a list.
-    grid_list = [[int(x) for x in grid.split()][i:i + width] for i in range(0, grid.count(" "), width)]
-            
-    # Create our grid as an object.
-    product_grid = ProductGrid(height, width, length, grid_list)
-    
-    # Step through rows.
-    for row in range(0, height):
-        # Step through columns.        
-        for column in range(0, width):
-            current_product = product_grid.FindProduct(row, column)
-            
-            if current_product > largest_product:
-                largest_product = current_product
-        
-    return largest_product
+import math
 
+# Let's just create a list of all prime numbers less than max.
+# To do this, we'll use the Sieve of Eratosthenes
+def SumOfPrimes(max):
+    product = 0
+    
+    # Let's create a container for our sieve.
+    # We begin by assuming that all numbers are prime.
+    # Note that our sieve technically identifies 0 and 1 as prime.
+    sieve = [True] * (max + 1)
+    
+    # Now we use the Sieve of Eratosthenes to rule out composite numbers.
+    # We begin with 2 and check all numbers up to the square root of max.
+    for x in range(2, math.floor(math.sqrt(max)) + 1):
+        # There is no need to check numbers less than x, so we find all
+        #    products with factors x and y >= x.
+        for y in range(x, (max // x) + 1):
+            # Any number x * y is by definition a composite number. 
+            sieve[(x * y)] = False
+    
+    # Now we check out sieve to see what prime numbers came up.
+    for prime in range(2, len(sieve)):
+        if sieve[prime]:
+            product += prime
+    
+    return product
 
-
-# As usual, we make a function call.
-print(FindLargestGridProduct(20, 20, 4, '08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'))
+# Our modular function call.
+print(SumOfPrimes(2000000))
